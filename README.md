b44c3793f128f1aa167c8f052b264b35ab4cdb73
manojaxis <manoj@axisrooms.com>
2024-07-22 00:31:52 +0530

    All code tested

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 Tools----> manage library ---->
 IRremote : 2.6.0 install
+
+
+IR recever
+1. V+
+2. GND
+3. Out - 11 pin
+
+IR Led
++ve = Pin 3 (Default)
+-ve = GND
--- ir_send_fan/ir_send_fan.ino
+++ ir_send_fan/ir_send_fan.ino
@@ -1,7 +1,7 @@
 #include <IRremote.h>
 
-const int irLedPin = 3; // Pin connected to IR LED (or transistor base)
-IRsend irsend(irLedPin);
+// Pin 3 connected to IR LED + (or transistor base)
+IRsend irsend;
 
 void setup() {
   // Initialize IR sender
@@ -11,6 +11,6 @@ void loop() {
   // Send the IR signal using the code received from CFD12E
   irsend.sendNEC(0xCF8976, 32); //Fan Power button
   delay(50000);
-  irsend.sendNEC(0xCFD12E, 32); // Example NEC code
-  delay(20000); // Wait 20 seconds before sending again
+  //irsend.sendNEC(0xCFD12E, 32); // Example NEC code
+  //delay(20000); // Wait 20 seconds before sending again
 }
